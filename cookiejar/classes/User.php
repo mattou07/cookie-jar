eb6acbf780cfcbbf87e45792634a0f5ccc8e2cb6
Sir Matthew L Hart <matthew258@gmail.com>
2015-11-05 20:13:02 +0000

    Added constructor to User class

--- cookiejar/classes/User.php
+++ cookiejar/classes/User.php
@@ -8,4 +8,53 @@ namespace WiseBiscuit\CookieJar;
  */
 class User {
 
+    const ACCOUNT_GUEST = 0;
+    const ACCOUNT_USER = 1;
+    const ACCOUNT_ADMIN = 2;
+
+    /**
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    public $username;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var string
+     */
+
+    public $surname;
+
+    /**
+     * @var int
+     */
+
+    private $user_level;
+
+    /**
+     * @param int $id
+     * @param string $username
+     * @param string $name
+     * @param string $surname
+     * @param int $user_level
+     */
+
+    public function __construct($id, $username, $name, $surname, $user_level= User::ACCOUNT_USER)
+    {
+        $this ->id = $id;
+        $this ->username = $username;
+        $this ->name = $name;
+        $this ->surnname = $surnname;
+        $this ->user_level = $user_level;
+
+    }
+
 }
